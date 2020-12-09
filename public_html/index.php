4940f7511d576653313df7f7111e66ed006e902b
Alex <proskurin.sasha78@gmail.com>
2020-12-09 11:50:18 +0300

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,34 @@
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+
+        <title>
+            Главная | Строй43
+        </title>
+        <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body class="homepage">
+     <div class="header">
+         <a href class="header_logo">
+             <img src="/logo.png" alt="извините все пиздец">
+         </a>
+         <div class="menu">
+             <ul>
+                 <li></li>
+                 <li></li>
+                 <li></li>
+                 <li></li>
+             </ul>
+         </div>
+         <div class="social_vk">
+
+
+         </div>
+         <div class="telephone">
+
+
+
+         </div>
+     </div>
+    </body>
+</html>
