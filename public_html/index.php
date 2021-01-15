a01ce9f4f25b04caa744e17f89f42dae0982cd3e
trueotmor <proskurin.sasha78@gmail.com>
2021-01-15 15:02:02 +0300

    addn

--- public_html/index.php
+++ public_html/index.php
@@ -14,10 +14,10 @@
          </a>
          <div class="menu">
              <ul>
-                 <li></li>
-                 <li></li>
-                 <li></li>
-                 <li></li>
+                 <li>1</li>
+                 <li>2</li>
+                 <li>3</li>
+                 <li>4</li>
              </ul>
          </div>
          <div class="social_vk">
